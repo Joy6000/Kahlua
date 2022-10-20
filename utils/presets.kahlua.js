f32afd462193e9f88d427ca55dc5018a15f6f359
Joy6000 <fnafgamer545@gmail.com>
2022-10-20 02:04:35 +0200

    chore: refactor presets.kahlua.js

--- utils/presets.kahlua.js
+++ utils/presets.kahlua.js
@@ -11,12 +11,23 @@ exports.default = eventModule({
 
 // COMMANDS //
 const { CommandType, commandModule } = require('@sern/handler');
-const { publish, ownerOnly } = require("../../../utils/plugins.js")
+const { publish, ownerOnly, permcheck } = require("../../../utils/plugins.js")
+const { Model } = require("../../models.sql")
+const { ApplicationCommandOptionType } = require("discord.js")
+const { PermissionsBitField: { FLAGS: perms } } = require("discord.js")
 
 exports.default = commandModule({
-    name: 'TBD',
-    description: 'TBD',
-    plugins: [publish(), ownerOnly()],
+    name: 'name-of-command',
+    description: 'description-of-command',
+    plugins: [publish(), ownerOnly(), permcheck(perms.PermissionHere)],
+    options: [
+        {
+            name: "option-name",
+            description: "description-of-option",
+            type: ApplicationCommandOptionType.OptionTypeHere,
+            required: true
+        }
+    ],
     type: CommandType.Slash,
     async execute(ctx, args) {
           // CODE HERE... //
