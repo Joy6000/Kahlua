c825469505b59b6d38ec4bb2e12d1053563949db
Joy6000 <fnafgamer545@gmail.com>
2022-11-06 18:54:34 -0800

    final(amend): Guild Join Alert

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -2,7 +2,9 @@ const { EventType, eventModule } = require("@sern/handler");
 exports.default = eventModule({
     name: "guildCreate",
     type: EventType.Discord,
-    async execute(guild) {
+    async execute(guild, client) {
         console.log(`Joined Guild "${guild.name}" (${guild.id})`)
+        let channel = await guild.client.channels.fetch('1039008507174854686')
+        channel.send(`Joined Guild "${guild.name}" (${guild.id})`)
     }
 })
