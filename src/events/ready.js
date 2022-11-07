35a8c01919bd7585892e66547986f4e2f3f81e25
Joy6000 <fnafgamer545@gmail.com>
2022-11-06 19:04:59 -0800

    final(amend): Guild Join Alert

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,13 +1,13 @@
 const { EventType, eventModule } = require("@sern/handler");
-const {ActivityType} = require("discord.js");
+const { ActivityType } = require("discord.js")
 exports.default = eventModule({
-    name: "guildCreate",
+    name: "ready",
     type: EventType.Discord,
-    async execute(guild, client) {
-        console.log(`Joined Guild "${guild.name}" (${guild.id})`)
-        let channel = await guild.client.channels.fetch('1039008507174854686')
-        channel.send(`Joined Guild "${guild.name}" (${guild.id})`)
+    async execute(client) {
+        console.log(`${client.guilds.cache.size} servers`)
+
         client.user.setPresence({ activities: [{ name: `${client.guilds.cache.size} Servers` }], status: 'online' });
         client.user.setActivity('discord.js', { type: ActivityType.Watching });
+
     }
 })
